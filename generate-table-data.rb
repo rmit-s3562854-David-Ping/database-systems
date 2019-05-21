74b20de962501e91ae3ce4962b272ad445be3c5f
Luke Mason <rmit-s3630120-luke-mason>
2019-05-21 11:31:59 +1000

    Added stuff

--- generate-table-data.rb
+++ generate-table-data.rb
@@ -3,15 +3,42 @@
 require 'csv.rb'
 require 'time.rb'
 
+# DATASET COLUMNS
+# COL1_NAME = 'device_id'
+# COL2_NAME = 'arrival_time'
+# COL3_NAME = 'departure_time'
+# COL4_NAME = 'duration_seconds'
+# COL5_NAME = 'street_marker'
+# COL6_NAME = 'parking_sign'
+# COL7_NAME = 'area'
+# COL8_NAME = 'street_id'
+# COL9_NAME = 'street_name'
+# COL10_NAME = 'between_street_1'
+# COL11_NAME = 'between_street_2'
+# COL12_NAME = 'side_of_street'
+# COL13_NAME = 'in_violation'
+
+DEVICE_ID_COL = 0
+ARRIVAL_TIME_COL = 1
+DEPARTURE_TIME_COL = 2
+DURATION_SECONDS_COL = 3
+STREET_MARKER_COL = 4
+PARKING_SIGN_COL = 5
+AREA_COL = 6
+STREET_ID_COL = 7
+STREET_NAME_COL = 8
+B_STREET_1_COL = 9
+B_STREET_2_COL = 10
+SIDE_OF_STREET_COL = 11
+IN_VIOLATION_COL = 12
+
 AREA_OUTPUT_FILE = 'area.csv'
-PARKING_SIGN_OUTPUT_FILE = 'parking-sign.csv'
 STREET_OUTPUT_FILE = 'street.csv'
-STREET_SEGMENT_OUTPUT_FILE = 'street-segment.csv'
+SEGMENT_OUTPUT_FILE = 'segment.csv'
+STREET_SIDE_SEGMENT_OUTPUT_FILE = 'street-side-segment.csv'
+PARKING_SIGN_OUTPUT_FILE = 'parking-sign.csv'
 PARKING_BAY_OUTPUT_FILE = 'parking-bay.csv'
 PARKING_EVENT_OUTPUT_FILE = 'parking-event.csv'
-
-
-
 
 
 if ARGV.length != 1
@@ -27,40 +54,93 @@ if !File.file?(source_file) || !File.exist?(source_file)
 end
 
 File.open(AREA_OUTPUT_FILE, 'w') do |area_file|
-  File.open(PARKING_SIGN_OUTPUT_FILE, 'w') do |parking_sign_file|
-    File.open(STREET_OUTPUT_FILE, 'w') do |street_file|
-      File.open(STREET_SEGMENT_OUTPUT_FILE, 'w') do |street_segment_file|
-        File.open(PARKING_BAY_OUTPUT_FILE, 'w') do |parking_bay_file|
-          File.open(PARKING_EVENT_OUTPUT_FILE, 'w') do |parking_event_file|
-            street_markers = {}
-            device_id_arrival_time = {}
+  File.open(STREET_OUTPUT_FILE, 'w') do |street_file|
+    File.open(SEGMENT_OUTPUT_FILE, 'w') do |segment_file|
+      File.open(STREET_SIDE_SEGMENT_OUTPUT_FILE, 'w') do |street_side_segment_file|
+        File.open(PARKING_SIGN_OUTPUT_FILE, 'w') do |parking_sign_file|
+          File.open(PARKING_BAY_OUTPUT_FILE, 'w') do |parking_bay_file|
+            File.open(PARKING_EVENT_OUTPUT_FILE, 'w') do |parking_event_file|
+              area_names = {}
+              street_ids = {}
+              segment_keys = {}
+              side_segments = {}
+              parking_sign_ids = {}
+              street_markers = {}
+              device_id_arrival_time = {}
 
-            CSV.foreach(source_file, {:headers => true}) do |row|
-              if !street_markers.has_key?(row[4]) && row[4] != nil
-                parking_bay_file.write("'#{row[4]}','#{row[6]}','#{row[8]}','#{row[9]}','#{row[10]}'\n")
-                street_markers[row[4]] = true
+              CSV.foreach(source_file, {:headers => true}) do |row|
+
+                # Area
+                area_name = row[AREA_COL]
+                if !area_names.has_key?(area_name) && area_name != nil
+                  # AREA_ID | AREA
+                  area_file.write("#{area_names.length},'#{area_name}'\n")
+                  area_names[area_name] = true
+                end
+
+                # Street
+                street_id = row[STREET_ID_COL]
+                if !street_ids.has_key?(street_id) && street_id != nil
+                  # STREET_ID | STREET_NAME | AREA_ID
+                  street_file.write("#{street_id}, '#{row[STREET_NAME_COL]}', '#{area_name}'")
+                  street_ids[street_id] = true
+                end
+
+                # Street segment
+                # Create 2 keys just incase the same segment comes in but back to front
+                between_street_1 = row[B_STREET_1_COL]
+                between_street_2 = row[B_STREET_2_COL]
+                segment_composite_key = "#{between_street_1}-#{between_street_2}"
+                segment_composite_key2 = "#{between_street_2}-#{between_street_1}"
+                if !segment_keys.has_key?(segment_composite_key) && !segment_keys.has_key?(segment_composite_key2) && between_street_1 != nil && between_street_2 != nil
+                  segment_file.write("'#{between_street_1}', '#{between_street_2}'")
+                  segment_keys[segment_composite_key] = true
+                  segment_keys[segment_composite_key2] = true
+                end
+
+                # Street Side Segment
+                side_of_street = row[SIDE_OF_STREET_COL]
+                side_segment = "#{street_id}-#{side_of_street}"
+                if !side_segments.has_key?(side_segment) && street_id != nil && side_of_street != nil
+                  street_side_segment_file.write("#{street_id}, '#{side_of_street}', '#{between_street_1}', '#{between_street_2}'")
+                end
+
+                # Parking Sign
+                parking_sign = row[PARKING_SIGN_COL]
+                if !parking_sign_ids.has_key?()
+
+                # Parking Bay
+
+                # Parking Event
+
+
+
+
+                if !street_markers.has_key?(row[4]) && row[4] != nil
+                  parking_bay_file.write("'#{row[4]}','#{row[6]}','#{row[8]}','#{row[9]}','#{row[10]}'\n")
+                  street_markers[row[4]] = true
+                end
+
+                if street_markers.has_key?(row[4])
+                  # FORMAT THE DATE AND TIME
+                  # FROM
+                  # 07/21/2017 11:42:28 PM
+                  # TO
+                  # 2017-07-21 23:42:28
+                  if row[1] != nil
+                    row[1] = Time.strptime(row[1], '%m/%d/%Y %T %p').strftime('%Y-%m-%d %T')
+                  end
+
+                  if row[2] != nil
+                    row[2] = Time.strptime(row[2], '%m/%d/%Y %T %p').strftime('%Y-%m-%d %T')
+                  end
+                  # Concatenate the device id and arrival time and add to the hash, if it already exists then ignore this row
+                  if row[0] != nil && row[1] != nil && !device_id_arrival_time.has_key?(row[0] + row[1])
+                    parking_event_file.write("#{row[0]},#{row[1]},#{row[2]},#{row[3]},'#{row[4]}','#{row[5]}',#{row[7]},#{row[11]},#{row[12]}\n")
+                    device_id_arrival_time[row[0] + row[1]] = true
+                  end
+                end
               end
-
-              if street_markers.has_key?(row[4])
-                # FORMAT THE DATE AND TIME
-                # FROM
-                # 07/21/2017 11:42:28 PM
-                # TO
-                # 2017-07-21 23:42:28
-                if row[1] != nil
-                  row[1] = Time.strptime(row[1], '%m/%d/%Y %T %p').strftime('%Y-%m-%d %T')
-                end
-
-                if row[2] != nil
-                  row[2] = Time.strptime(row[2], '%m/%d/%Y %T %p').strftime('%Y-%m-%d %T')
-                end
-                # Concatenate the device id and arrival time and add to the hash, if it already exists then ignore this row
-                if row[0] != nil && row[1] != nil && !device_id_arrival_time.has_key?(row[0] + row[1])
-                  file.write("#{row[0]},#{row[1]},#{row[2]},#{row[3]},'#{row[4]}','#{row[5]}',#{row[7]},#{row[11]},#{row[12]}\n")
-                  device_id_arrival_time[row[0] + row[1]] = true
-                end
-              end
-
             end
           end
         end
@@ -69,17 +149,6 @@ File.open(AREA_OUTPUT_FILE, 'w') do |area_file|
   end
 end
 
-# DATASET COLUMNS
-# COL1_NAME = 'device_id'
-# COL2_NAME = 'arrival_time'
-# COL3_NAME = 'departure_time'
-# COL4_NAME = 'duration_seconds'
-# COL5_NAME = 'street_marker'
-# COL6_NAME = 'parking_sign'
-# COL7_NAME = 'area'
-# COL8_NAME = 'street_id'
-# COL9_NAME = 'street_name'
-# COL10_NAME = 'between_street_1'
-# COL11_NAME = 'between_street_2'
-# COL12_NAME = 'side_of_street'
-# COL13_NAME = 'in_violation'
+
+
+
--- tables.sql
+++ tables.sql
@@ -1,3 +1,7 @@
-CREATE TABLE parking_bay (street_marker VARCHAR(10) NOT NULL,area VARCHAR(30),street_name VARCHAR(30),between_street_1 VARCHAR(30),between_street_2 VARCHAR(30),PRIMARY KEY (street_marker))
-CREATE TABLE parking_event (device_id INTEGER NOT NULL,arrival_time TIMESTAMP NOT NULL,departure_time TIMESTAMP,duration_seconds BIGINT,street_marker VARCHAR(10) NOT NULL,parking_sign VARCHAR(50),street_id INTEGER,side_of_street INTEGER CHECK(side_of_street > 0 AND side_of_street <= 5),in_violation BOOLEAN,FOREIGN KEY (street_marker) REFERENCES parking_bay(street_marker),PRIMARY KEY (device_id, arrival_time))
-
+create table Area(area_id unsigned smallint primary key, area varchar(20))
+create table Street(street_id unsigned smallint primary key, street_name varchar(35), area_id unsigned smallint references Area(area_id))
+create table Segment(between_street_1 varchar(35) not null, between_street_2 varchar(35) not null, primary key (between_street_1, between_street_2))
+create table StreetSideSegment(street_id unsigned smallint references Street(street_id), side_of_street unsigned tinyint not null, between_street_1 unsigned int references Segment(between_street_1), between_street_2 unsigned int references Segment(between_street_2), primary key (street_id, side_of_street))
+create table Sign(sign_id unsigned int primary key, sign_details varchar(50))
+create table ParkingBay(street_marker varchar(6) primary key, device_id unsigned int, sign_id unsigned int references Sign(sign_id), side_of_street unsigned tinyint references StreetSegmentSide(side_of_street), street_id unsigned smallint references StreetSegmentSide(street_id))
+create table ParkingTime(arrival_time long not null, street_marker varchar(6) not null references ParkingBay(street_marker), departure_time long, duration unsigned int, in_violation boolean, primary key (street_marker, arrival_time))
