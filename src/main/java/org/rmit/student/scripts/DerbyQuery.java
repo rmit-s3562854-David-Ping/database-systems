c5df53f686ce4f73dd0bc8981df1404bc19d4472
Luke Mason <rmit-s3630120-luke-mason>
2019-05-27 19:21:47 +1000

    Made pom use derby query

--- src/main/java/org/rmit/student/scripts/DerbyQuery.java
+++ src/main/java/org/rmit/student/scripts/DerbyQuery.java
@@ -1,10 +1,7 @@
 package org.rmit.student.scripts;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DerbyQuery {
